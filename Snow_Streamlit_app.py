6bb25ab997ed2308ce48fbd9e4064e23bd2afd99
Chakradhar Ananthoju <110176030+ChakradharAnanthoju@users.noreply.github.com>
2022-08-03 19:32:09 +0530

    Update Snow_Streamlit_app.py

--- Snow_Streamlit_app.py
+++ Snow_Streamlit_app.py
@@ -1,2 +1,8 @@
 import streamlit
+
 streamlit.title('My Parents had a Healthy Diet')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach 3 & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
