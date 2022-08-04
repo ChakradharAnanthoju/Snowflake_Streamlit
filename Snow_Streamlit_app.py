2d3319ba20ec05ebe6487ba15cf73ec6b43bf80c
Chakradhar Ananthoju <110176030+ChakradharAnanthoju@users.noreply.github.com>
2022-08-04 10:28:09 +0530

    Update Snow_Streamlit_app.py

--- Snow_Streamlit_app.py
+++ Snow_Streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My Moms New Healthy Diet')
+streamlit.title('My Mom"'"s New Healthy Diet')
 
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
