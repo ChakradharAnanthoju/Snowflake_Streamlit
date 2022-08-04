3afe7f9ceafc4e9658a2dd6307b49ea1a84646f2
Chakradhar Ananthoju <110176030+ChakradharAnanthoju@users.noreply.github.com>
2022-08-04 12:31:49 +0530

    Update Snow_Streamlit_app.py

--- Snow_Streamlit_app.py
+++ Snow_Streamlit_app.py
@@ -11,7 +11,8 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas
-my_friut_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Lets put a pick list here so they can pick the fruit they want to include
