4556a741840e17a03b560eaf3cc4bf5b31742d97
Chakradhar Ananthoju <110176030+ChakradharAnanthoju@users.noreply.github.com>
2022-08-04 10:53:39 +0530

    Update Snow_Streamlit_app.py
    pandas.read_csv updated

--- Snow_Streamlit_app.py
+++ Snow_Streamlit_app.py
@@ -11,6 +11,6 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas
-my_friut_list = pandas_read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_friut_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_friut_list)
   
