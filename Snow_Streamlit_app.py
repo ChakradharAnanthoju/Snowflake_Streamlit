c4f80b85eec7637e3edf8ed4d240ec43e59479b6
Chakradhar Ananthoju <110176030+ChakradharAnanthoju@users.noreply.github.com>
2022-08-04 10:51:01 +0530

    Update Snow_Streamlit_app.py

--- Snow_Streamlit_app.py
+++ Snow_Streamlit_app.py
@@ -12,5 +12,5 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas
 my_friut_list = pandas_read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_friut_list):
+streamlit.dataframe(my_friut_list)
   
